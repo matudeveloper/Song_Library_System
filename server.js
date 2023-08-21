ff3c793a2690803c06a2c54440681659e126ec13
martin <martin.laan@voco.ee>
2023-08-21 16:01:20 +0300

    As a user I can sign in and out [Delivers #185804917]"

--- server.js
+++ server.js
@@ -9,155 +9,42 @@ const session = require('express-session');
 const path = require('path');
 const jwt = require('jsonwebtoken');
 const LocalStorage = require('node-localstorage').LocalStorage;
+const cookieParser = require('cookie-parser');
 
 
 // Initialize Express app
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-//const swaggerDocument = yamljs.load('./swagger.yaml')
-//app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
-
-
 // Parse JSON data
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 // Assuming you have the necessary middleware for parsing JSON body.
 app.use(express.json());
+app.use(cookieParser());
 
 // Initialize Prisma client
 const prisma = new PrismaClient();
 
-// Setup express-session middleware
-app.use(session({
-    secret: '12345678', // Replace with a secure secret key
-    resave: false,
-    saveUninitialized: true,
-}));
 
-// Middleware to authenticate token
-// Middleware to check authentication and restrict access
-function requireAuth(req, res, next) {
-    //console.log(req.session.isAuthenticated);
-    if (req.session.isAuthenticated) {
-        // User is authenticated, proceed to the next middleware or route handler
-        //next();
-        console.log('you are signed in');
-    } else {
-        // User is not authenticated, redirect to login page
-        //res.redirect('/');
-    }
-}
-
-// Authorization middleware
-const authorizeRequest = async (req, res, next) => {
-
-    try {
-        //console.log(req.sessionID);
-        if (req.sessionID && req.sessionID === 'undefined') {
-            // User is authenticated, proceed to the next middleware or route handler
-            //next();
-            //console.log('you are signed in');
-        } else {
-            // User is not authenticated, redirect to login page
-            //res.redirect('/');
-        }
-
-        // Validate session
-        if (!req.headers.authorization) {
-            return res.status(401).json({ error: 'Authorization header required'});
-        }
-
-        // Validate extract session format
-        if (!req.headers.authorization.startsWith('Bearer') || req.headers.authorization.split(' ').length !== 2) {
-            return res.status(401).json({ error: 'Invalid authorization header format'});
-        }
-
-        // Extract sessionToken
-        const sessionToken = req.headers.authorization.split(' ')[1];
-
-        const session = await prisma.session.findUnique({
-            where: {
-                sessionToken: sessionToken,
-            },
-        });
-
-        if (!session) {
-            return res.status(401).json({ error: 'Invalid session token'});
-        }
-        console.log(session);
-        // Add user to request
-        const user = await prisma.user.findUnique({
-            where: {
-                id: session.userId,
-            },
-        });
-
-        // Validate user
-        if (!user) {
-            return res.status(401).json({ error: 'Invalid session token'});
-        }
-
-        // Add sessionToken to request
-        req.sessionToken = sessionToken;
-
-        next();
-    } catch (error) {
-        console.error('Authorization error:', error);
-        res.status(500).json({ error: 'Internal Server Error'});
-    }
-};
-
-/*
-// Serve dashboard page (protected with requireAuth middleware)
-app.get('/dashboard', authorizeRequest, (req, res) => {
-    // Assuming you have an HTML file named 'dashboard.html' in a 'views' directory
-    //res.sendFile(path.join(__dirname, 'Wiews', 'dashboard.html'));
-
-
-});
-*/
-// Dashboard Route (protected)
 // Dashboard Route (protected)
 app.get("/dashboard", async (req, res) => {
-    const authHeader = req.headers.authorization;
-    console.log(req.headers);
-    if (!authHeader/* || !authHeader.startsWith('Bearer ')*/) {
+    const token = req.cookies.authToken;
+
+    if (!token) {
         return res.status(401).json({ error: "Not Authorized" });
     }
 
-    const token = authHeader.split(' ')[1];
+    res.sendFile(path.join(__dirname, '/frontend/Views/dashboard.html')); // Adjust the path as needed
 
-    try {
-        //const decoded = jwt.verify(token, 'YOUR_SECRET_KEY');
-        const session = await prisma.session.findUnique({
-            where: { sessionToken: token }
-        });
 
-        if (!session) {
-            return res.status(401).json({ error: "Session not found. Please login again." });
-        }
 
-        // Send the dashboard file
-        res.sendFile(path.join(__dirname, '/frontend/Views/dashboard.html')); // Adjust the path as needed
-    } catch (err) {
-        console.error('Error in /dashboard:', err);
-        res.status(500).json({ error: 'An error occurred' });
-    }
 });
-/*
-// Serve login page
-app.get('/login', (req, res) => {
-    // Serve your login HTML file
-    res.sendFile(path.join(__dirname, '/frontend/Views/login.html')); // Adjust the path as needed
-});
-*/
+
 
 app.use(express.static(__dirname + '/frontend/public')); // Replace 'public' with the directory containing your static files
-//console.log(app.use(express.static(__dirname + '/frontend/public')))
 
-// Endpoint to handle user registration
-// Sign Up route
+
 // Register new user
 app.post('/users', async (req, res) => {
     const { email, password } = req.body;
@@ -214,21 +101,18 @@ app.post('/login', async (req, res) => {
             return res.status(401).json({ error: 'Invalid credentials' });
         }
 
-        // Generate a JWT token after verifying the password
-        const token = jwt.sign({ userId: user.id }, 'YOUR_SECRET_KEY');  // Change the secret key
+        if (user !== null) {
+            const token = jwt.sign({ userId: user.id }, 'YOUR_SECRET_KEY');
+            res.cookie('authToken', token, {
+                httpOnly: false,
+                expires: new Date(Date.now() + (3600 * 1000)),  // 1 hour expiration
+                secure: process.env.NODE_ENV === 'production',  // In production, make sure cookies are sent over HTTPS
+                sameSite: 'strict'
+            });
+            res.status(201).json({ message: 'Login successful' });
+        }
 
-        const session = await prisma.session.create({
-            data: {
-                userId: user.id,
-                sessionToken: token,
-                expires: new Date(Date.now() + (3600 * 1000)) // 1 hour from now
-            },
-        });
 
-        // Return session token to the client
-        res.status(201).json({
-            sessionToken: session.sessionToken
-        });
 
     } catch (error) {
         console.error('Error processing /login:', error);
@@ -236,19 +120,11 @@ app.post('/login', async (req, res) => {
     }
 });
 
+app.post('/logout', (req, res) => {
+    res.clearCookie('authToken');
+    res.json({ message: 'Logged out' });
+});
 
-app.delete('/login', authorizeRequest, async (req, res) => {
-
-    // Delete session
-    await prisma.session.delete({
-        where: {
-            sessionToken: req.sessionToken
-        }
-    })
-
-    // Return session
-    res.status(204).end()
-})
 
 // Serve Swagger documentation
 const swaggerDocument = yaml.load('swagger.yaml');
